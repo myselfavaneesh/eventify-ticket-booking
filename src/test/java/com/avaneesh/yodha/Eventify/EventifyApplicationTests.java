7150ba9a81ef657b19bb920871337eac9981eafd
myselfavaneesh <avaneeshjaiswal2k25@gmail.com>
2025-09-13 23:37:47 +0530

    Initial commit

---
+++ src/test/java/com/avaneesh/yodha/Eventify/EventifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avaneesh.yodha.Eventify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
